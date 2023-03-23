d21387bbd647320a8cc65f06e640925cf778b83a
basseysophia <mightyleadproperty@gmail.com>
2023-03-23 08:58:25 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar"
+*/ To print the string _putchar on screen
+*
+* Return 0 on success
+*/
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('/n')
+	
+
+	Return(0);
+}
