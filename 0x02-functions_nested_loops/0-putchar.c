aa1f653a130ff3333e19abb51ba3ad0a10dd9521
basseysophia <mightyleadproperty@gmail.com>
2023-03-28 07:56:36 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - This is the main function
+ * This is to print the the alphabets _putchar
+ * Return: 0 on sucess
+ */
+int main(void);
+
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	Return (0);
+
+}
