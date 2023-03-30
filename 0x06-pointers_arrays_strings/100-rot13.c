c6baac6e0d7edf05b809060ac777e7f7159d50f0
basseysophia <mightyleadproperty@gmail.com>
2023-03-30 15:50:55 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,12 +1,89 @@
-#include "main.h"
+#include "main.h"                                                                                                                    
 
-/**
- * print_buffer - add 2 strings.
- * @b: string1.
- * @size: string2.
- * Return: String with all letters in ROT13 base.
+                                                                                                                                     
+
+/**                                                                                                                                  
+
+ * rot13 - Encodes a string using rot13.                                                                                                    
+
+ *                                                                                                                                    
+
+ * Return: A pointer to the encoded string.                                                                                          
+
  */
-void print_buffer(char *b, int size)
-{
+
+char *rot13(char *str)                                                                                                                
+
+{                                                                                                                                    
+
+        int indx1 = 0, indx2;                                                                                                        
+
+        char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',                                                                            
+
+                             'G', 'H', 'I', 'J', 'K', 'L',                                                                            
+
+                             'M', 'N', 'O', 'P', 'Q', 'R',                                                                            
+
+                             'S', 'T', 'U', 'V', 'W', 'X',                                                                            
+
+                             'Y', 'Z', 'a', 'b', 'c', 'd',                                                                            
+
+                             'e', 'f', 'g', 'h', 'i', 'j',                                                                            
+
+                             'k', 'l', 'm', 'n', 'o', 'p',                                                                            
+
+                             'q', 'r', 's', 't', 'u', 'v',                                                                            
+
+                             'w', 'x', 'y', 'z'};                                                                                    
+
+        char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',                                                                            
+
+                             'T', 'U', 'V', 'W', 'X', 'Y',                                                                            
+
+                             'Z', 'A', 'B', 'C', 'D', 'E',                                                                            
+
+                             'F', 'G', 'H', 'I', 'J', 'K',                                                                            
+
+                             'L', 'M', 'n', 'o', 'p', 'q',                                                                            
+
+                             'r', 's', 't', 'u', 'v', 'w',                                                                            
+
+                             'x', 'y', 'z', 'a', 'b', 'c',                                                                            
+
+                             'd', 'e', 'f', 'g', 'h', 'i',                                                                            
+
+                             'j', 'k', 'l', 'm'};                                                                                    
+
+                                                                                                                                     
+
+        while (str[indx1])                                                                                                            
+
+        {                                                                                                                            
+
+                for (indx2 = 0; indx2 < 52; indx2++)                                                                                  
+
+                {                                                                                                                    
+
+                        if (str[indx1] == alphabet[indx2])                                                                            
+
+                        {                                                                                                            
+
+                                str[indx1] = rot13key[indx2];                                                                        
+
+                                break;                                                                                                
+
+                        }                                                                                                            
+
+                }                                                                                                                    
+
+                                                                                                                                     
+
+                indx1++;                                                                                                              
+
+        }                                                                                                                            
+
+                                                                                                                                     
+
+        return (str);                                                                                                                
 
 }
