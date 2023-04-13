9bcd6ce4a491c7568b36bbc018a52ba4d8a7aec6
basseysophia <mightyleadproperty@gmail.com>
2023-04-13 04:41:29 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,30 +1,18 @@
-include "main.h"
-
+#include "main.h"
 
 /**
-
- * print_numbers - print 0-9
-
- * return: void
-
+ * print_numbers - print numbers 0-9
+ *
+ * Return:always 0
  */
 
-
 void print_numbers(void)
-
 {
-
-        char c;
-
-
-        for (c = '0'; c <= '9'; c++)
-
-        {
-
-                _putchar(c);
-
-        }
-
-        _putchar('\n');
-
+int num;
+for (num = 0; num <= 9; num++)
+{
+	_putchar(num + '0');
 }
+	_putchar('\n');
+}
+
