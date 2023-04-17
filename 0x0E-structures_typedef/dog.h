b3bd5ca06843f6a5ed25fb90963ec9ed7ee4dc61
basseysophia <mightyleadproperty@gmail.com>
2023-04-17 08:43:18 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,38 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
-
-
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
-
-
-	/**
-	 * dog_t - typedef for struct dog
-	 */
-	typedef struct dog dog_t;
-
-
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
-
-
-	#endif
+/**
+ * struct dog - list dates about pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+ * dog - rename strucut with typedef
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* DOG_H */
 
 
 
